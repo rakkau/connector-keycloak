3c5bdc4f8a33c8e893592fbf792b82ade820b146
Javier <javier.aleparedes@gmail.com>
2026-01-19 14:14:01 -0300

    fix version 26.4

--- src/main/java/jp/openstandia/connector/keycloak/rest/KeycloakAdminRESTAdminClient.java
+++ src/main/java/jp/openstandia/connector/keycloak/rest/KeycloakAdminRESTAdminClient.java
@@ -15,12 +15,16 @@
  */
 package jp.openstandia.connector.keycloak.rest;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
+import com.fasterxml.jackson.jakarta.rs.json.JacksonJsonProvider;
 import jp.openstandia.connector.keycloak.KeycloakClient;
 import jp.openstandia.connector.keycloak.KeycloakConfiguration;
 import org.identityconnectors.common.StringUtil;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.framework.common.exceptions.ConnectorException;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.jboss.resteasy.core.providerfactory.ResteasyProviderFactoryImpl;
@@ -59,51 +63,80 @@ public class KeycloakAdminRESTAdminClient implements KeycloakClient {
         try {
             RuntimeDelegate.getInstance();
         } catch (Throwable t) {
-            // Set the implementation directly as a workaround
             RuntimeDelegate.setInstance(new ResteasyProviderFactoryImpl());
         }
 
+        // ★ 1. ObjectMapper tolerante
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(
+                DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false
+        );
+
+        // ★ 2. Jackson provider con ese mapper
+        JacksonJsonProvider jacksonProvider = new JacksonJsonProvider(objectMapper);
+
         ResteasyClientBuilder resteasyClientBuilder = new ResteasyClientBuilderImpl();
         resteasyClientBuilder.connectionPoolSize(20);
+
+        // ★ 3. Registrar el provider
+        resteasyClientBuilder.register(jacksonProvider);
 
         // HTTP proxy configuration
         if (configuration.getHttpProxyHost() != null && configuration.getHttpProxyPort() != 0) {
             final String httpProxyHost = configuration.getHttpProxyHost();
 
-            if (StringUtil.isNotBlank(configuration.getHttpProxyUser()) && configuration.getHttpProxyPassword() != null) {
+            if (StringUtil.isNotBlank(configuration.getHttpProxyUser())
+                    && configuration.getHttpProxyPassword() != null) {
+
                 configuration.getHttpProxyPassword().access(s -> {
-                    String httpProxyHostWithAuth = String.format("%s:%s@%s",
+                    String httpProxyHostWithAuth = String.format(
+                            "%s:%s@%s",
                             configuration.getHttpProxyUser(),
                             String.valueOf(s),
-                            httpProxyHost);
+                            httpProxyHost
+                    );
 
-                    resteasyClientBuilder.defaultProxy(httpProxyHostWithAuth, configuration.getHttpProxyPort(), "http");
+                    resteasyClientBuilder.defaultProxy(
+                            httpProxyHostWithAuth,
+                            configuration.getHttpProxyPort(),
+                            "http"
+                    );
                 });
             } else {
-                resteasyClientBuilder.defaultProxy(httpProxyHost, configuration.getHttpProxyPort(), "http");
+                resteasyClientBuilder.defaultProxy(
+                        httpProxyHost,
+                        configuration.getHttpProxyPort(),
+                        "http"
+                );
             }
         }
+
+        // ★ 4. Construir UNA VEZ el ResteasyClient
+        ResteasyClient resteasyClient = resteasyClientBuilder.build();
 
         // grant_type=password mode
         if (configuration.getUsername() != null && configuration.getPassword() != null) {
             configuration.getPassword().access(s -> {
+
                 KeycloakBuilder adminClientBuilder = KeycloakBuilder.builder()
                         .serverUrl(configuration.getServerUrl())
                         .realm(configuration.getRealmName())
                         .grantType("password")
                         .username(configuration.getUsername())
                         .password(String.valueOf(s))
-                        .clientId(configuration.getClientId());
+                        .clientId(configuration.getClientId())
+                        // ★ 5. Usar SIEMPRE este cliente
+                        .resteasyClient(resteasyClient);
 
-                if(configuration.getClientSecret() != null) {
-                    configuration.getClientSecret().access(cs -> {
-                        adminClientBuilder.clientSecret(String.valueOf(cs));
-                    });
+                if (configuration.getClientSecret() != null) {
+                    configuration.getClientSecret().access(cs ->
+                            adminClientBuilder.clientSecret(String.valueOf(cs))
+                    );
                 }
 
-                adminClient = adminClientBuilder.resteasyClient(resteasyClientBuilder.build())
-                        .build();
+                adminClient = adminClientBuilder.build();
             });
+
         } else if (configuration.getClientSecret() != null) {
             configuration.getClientSecret().access(s -> {
                 adminClient = KeycloakBuilder.builder()
@@ -112,6 +145,8 @@ public class KeycloakAdminRESTAdminClient implements KeycloakClient {
                         .grantType("client_credentials")
                         .clientId(configuration.getClientId())
                         .clientSecret(String.valueOf(s))
+                        // ★ MISMO cliente REST aquí también
+                        .resteasyClient(resteasyClient)
                         .build();
             });
         }
@@ -122,6 +157,7 @@ public class KeycloakAdminRESTAdminClient implements KeycloakClient {
         this.clientRole = new KeycloakAdminRESTClientRole(instanceName, configuration, adminClient);
     }
 
+
     private RealmResource realm(String realmName) {
         return adminClient.realm(realmName);
     }
@@ -129,10 +165,14 @@ public class KeycloakAdminRESTAdminClient implements KeycloakClient {
     @Override
     public void test(String realmName) {
         try {
-            Boolean enabled = adminClient.realm(realmName).toRepresentation().isEnabled();
-            if (Boolean.TRUE != enabled) {
+            String realmDisplayName = adminClient.realm(realmName).toRepresentation().getDisplayName();
+            if(realmDisplayName.isEmpty()){
                 throw new ConnectorException("The keycloak realm isn't active.");
             }
+            /*Boolean enabled = adminClient.realm(realmName).toRepresentation().isEnabled();
+            if (Boolean.TRUE != enabled) {
+                throw new ConnectorException("The keycloak realm isn't active.");
+            }*/
         } catch (ProcessingException e) {
             // Keycloak admin-client might throw exception due to version mismatch...
 
